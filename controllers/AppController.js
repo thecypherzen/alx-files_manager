f999f8ace0ab47efdb5377808620af0533635ae3
William Inyam <sw.inyam@outlook.com>
2024-12-14 00:55:28 +0000

    build(afm): build basic mvc file structure for api

---
+++ controllers/AppController.js
@@ -0,0 +1,25 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+function getStatus(req, res) {
+  if (dbClient.isAlive() && redisClient.isAlive()) {
+    res.send({ redis: true, db: true });
+  } else {
+    res.status(500).send(
+      {
+        redis: redisClient.isAlive(),
+        db: dbClient.isAlive(),
+      }
+    );
+  }
+}
+
+function getStats(req, res) {
+  (async () => {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.send({ users, files });
+  })();
+}
+
+export { getStatus, getStats };
---
+++ server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import router from './routes/index';
+
+const app = express();
+app.use('/', router);
+
+
+app.listen(process.env.PORT || 5000);
