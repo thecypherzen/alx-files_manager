a7d66f94a7c7928e7b43e4021670fe2fca647a79
William Inyam <sw.inyam@outlook.com>
2024-12-15 07:02:53 +0000

    feat(afm:controllers): add /get/me route controller

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,10 +1,44 @@
 import { dbClient } from '../utils';
-import { createHash, dbUtils } from '../shared';
+import { cache, createHash, dbUtils } from '../shared';
 
+/**
+ * @function getMe - retrieves a user object based on object
+ * @param { object } req - the incoming request object
+ * @param { object } res - the outgoing response object
+ * @returns { object } the resolved response object:
+ * If user with token sent in header exists, the response payload
+ * is an object with the user's email and id. Else, it's
+ * a custom error object.
+ */
 async function getMe(req, res) {
-  console.log('getMe called...');
+  const token = req.get('x-token');
+  // check user exists
+  // case not exists
+  const userId = await cache.getUserId(`auth_${token}`);
+  if (!userId) {
+    return res.status(401).send({ error: 'Unauthorized' });
+  }
+  const user = await dbUtils.getUserById(userId);
+  if (!user) {
+    return res.status(401).send({ error: 'Unauthorized' });
+  }
+
+  // case exists
+  return res.send({ id: user._id.toString(), email: user.email });
 }
 
+/**
+ * @function postNew - adds a new user to the database based on
+ * their credentials(password and email) sent as payload.
+ * @param { object } req - the incoming request object
+ * @param { object } res - the outgoing response object
+ * @returns { object } - the response object such that:
+ *   - A custom error object is sent if  none or partial credentials were sent, a user with email already exists or an error occured
+ *     while performing a write into the database.
+ *   - A response object with payload containing new user's
+ *     email and id.
+ *
+ */
 async function postNew(req, res) {
   // ensure data was sent
   if (!req.body || !req.body.email) {
