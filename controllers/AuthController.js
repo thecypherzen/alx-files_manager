44558a983d3d184d22fbff2fcccb7b5f6f363130
William Inyam <sw.inyam@outlook.com>
2024-12-15 01:33:30 +0000

    fix(afm): correct async related issues in authcontroller

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,19 +1,21 @@
 import { v4 as uuid4 } from 'uuid';
 import { cache, createHash, dbUtils } from '../shared';
 
-function getConnect(req, res) {
-  let authorization = req.get('authorization');
+async function getConnect(req, res) {
+ let authorization = req.get('authorization');
   if (!authorization) {
     return res.status(401)
       .send({ error: 'Unauthorized' });
   }
-  console.log(authorization);
+
   [, authorization] = authorization.split(' ');
   authorization = Buffer.from(authorization, 'base64')
     .toString('utf-8');
   const [email, password] = authorization.split(':');
   const pwdHash = createHash(password);
-  const user = dbUtils.getUserByCred({ email, password: pwdHash });
+  const user = await dbUtils.getUserByCred(
+    { email, password: pwdHash },
+  );
 
   if (!user) {
     return res.status(401).send({ error: 'Unauthorized' });
@@ -21,12 +23,11 @@ function getConnect(req, res) {
 
   const authToken = uuid4();
   const key = `auth_${authToken}`;
-  const value = cache.set(key, authToken, 86400);
+  const value = await cache.set(key, user._id.toString(), 86400);
   console.log('set response: ', value);
 
   return res.send({ token: authToken });
 }
 
-const someFunc = null;
 
-export { getConnect, someFunc };
+export { getConnect, getDisconnect };
